feb646d4c2690945e667702f37b659d25666834e
Tobias Norton <tnorton20@yahoo.com>
2018-11-28 20:46:12 -0500

    Character names displaying, next and previous buttons work dynamically

--- src/App.js
+++ src/App.js
@@ -1,28 +1,147 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+// import logo from './logo.svg'
+import './App.css'
+import axios from 'axios'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      characterObjects: [],
+      pageNumber: 1,
+      selectedCharacter: undefined
+    }
+  }
+
+  nextButton = () => {
+    if (this.state.pageNumber < 9) {
+      return (
+        <button className="list-button" onClick={this.nextCharacterList}>
+          More Characters
+        </button>
+      )
+    } else {
+      return
+    }
+  }
+
+  nextCharacterList = () => {
+    this.setState(
+      {
+        pageNumber: (this.state.pageNumber += 1)
+      },
+      () => {
+        axios.get(`https://swapi.co/api/people/?page=${this.state.pageNumber}`).then(response => {
+          console.log(response.data.results)
+          this.setState({
+            characterObjects: response.data.results
+          })
+        })
+      }
+    )
+  }
+
+  previousButton = () => {
+    if (this.state.pageNumber > 1) {
+      return (
+        <button className="list-button" onClick={this.previousList}>
+          Previous
+        </button>
+      )
+    }
+  }
+
+  previousList = () => {
+    this.setState(
+      {
+        pageNumber: (this.state.pageNumber -= 1)
+      },
+      () => {
+        axios.get(`https://swapi.co/api/people/?page=${this.state.pageNumber}`).then(response => {
+          console.log(response.data.results)
+          this.setState({
+            characterObjects: response.data.results
+          })
+        })
+      }
+    )
+  }
+
+  selectCharacter = event => {
+    // axios.get(`https://swapi.co/api/people/?page=${this.state.pageNumber}`).then(response => {
+    //   console.log(response.data.results)
+    this.setState(
+      {
+        selectedCharacter: event.target.value
+      },
+      () => {
+        console.log(this.state.selectedCharacter)
+      }
+    )
+    // })
+    // displayCharacterProfile()
+  }
+
+  // displayCharacterProfile = () => {
+  //   console.log(response.data.results)
+  // }
+
+  // previousButtonAppears = () => {
+  //   if (this.state.pageNumber === 9) {
+  //     return <button onClick={this.previousList}>Previous</button>
+  //   }
+  // }
+
+  // characterListButton = () => {
+  //   if (this.state.pageNumber < 9) {
+  //     return (
+  //       <button className="more-characters" onClick={this.nextCharacterList}>
+  //         More Characters
+  //       </button>
+  //     )
+  //   } else {
+  //     return <button onClick={this.previousList}>Previous</button>
+  //   }
+  // }
+
+  componentDidMount = () => {
+    axios.get('https://swapi.co/api/people').then(response => {
+      this.setState({
+        characterObjects: response.data.results
+      })
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Star Wars Characters</h1>
+        <h3>Choose a character to display their profile!</h3>
+        <section className="character-selection">
+          <ul>
+            {this.state.characterObjects.map((characterObject, index) => {
+              return (
+                <li value={characterObject.name} onClick={this.selectCharacter} key={index}>
+                  {characterObject.name}
+                </li>
+              )
+            })}
+          </ul>
+          <div className="buttons-container">
+            {this.nextButton()}
+            {this.previousButton()}
+          </div>
+          {/* <button className="more-characters" onClick={this.nextCharacterList}>
+            More Characters
+          </button> */}
+          {/* {this.previousButtonAppears()} */}
+
+          <div>{this.displayCharacterProfile}</div>
+        </section>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
